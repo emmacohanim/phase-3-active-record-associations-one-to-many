ef0bb1b3304c7c6bc90f9ceec63bd972de421d26
Emma Cohanim <emmacohanim@gmail.com>
2022-09-07 13:58:31 -0700

    commit tree a9af43a3e30b9aa5ca0a8a73503efb818fd9a54e

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
-  
+    has_many :reviews
+  # each game has many reviews
+    # the one has no dependencies, so we should do this first
+    # symbol passed must be plural
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
-  
+    belongs_to :game
+  # each game has many reviews
+  # symbol passed must be singular
 end
